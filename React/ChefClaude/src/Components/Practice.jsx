475ee7bfe8b9a02716a96d4f32380a3669551baa
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-17 00:47:03 +0530

    Practice state with array as state value

---
+++ React/ChefClaude/src/Components/Practice.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+export default function Practice() {
+    const [myFavouriteThings, setmyFavouriteThings] = useState([]);
+
+    const allFavoriteThings = [
+        "💦🌹",
+        "😺",
+        "💡🫖",
+        "🔥🧤",
+        "🟤🎁",
+        "🐴",
+        "🍎🥧",
+        "🚪🔔",
+        "🛷🔔",
+        "🥩🍝",
+    ];
+
+    function addFavoriteThing() {
+        setmyFavouriteThings((prevThings) => {
+            /* if (prevThings.length === 0) {
+                let [firstElement] = allFavoriteThings;
+                return firstElement;
+            }
+
+            if (prevThings.length === allFavoriteThings.length) {
+                return prevThings;
+            }
+
+            const index = allFavoriteThings.indexOf(
+                prevThings.find(prevThings.length - 1)
+            );
+
+            if (index < myFavouriteThings.length - 1) {
+                return [...prevThings, allFavoriteThings[index + 1]];
+            } */
+
+            if (prevThings.length < allFavoriteThings.length) {
+                return [...prevThings, allFavoriteThings.at(prevThings.length)];
+            } else {
+                return prevThings;
+            }
+        });
+    }
+
+    const thingsElements = myFavouriteThings.map((thing, index) => (
+        <p className="mt-2 mb-2" key={index}>
+            {thing}
+        </p>
+    ));
+
+    return (
+        <main className="w-screen h-screen text-center">
+            <button
+                className="mt-6 bg-blue-400 border-2 border-amber-400 rounded-xl w-24 h-10 hover:bg-blue-700"
+                onClick={addFavoriteThing}
+            >
+                Add item
+            </button>
+            <section
+                className="w-screen flex flex-col items-center"
+                aria-live="polite"
+            >
+                {thingsElements}
+            </section>
+        </main>
+    );
+}
