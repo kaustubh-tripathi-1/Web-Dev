c2f48fc9f5257b6560ab6eebc739d0573294ff4b
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-17 08:23:51 +0530

    Update App.jsx with diff. compos

--- React/ChefClaude/src/App.jsx
+++ React/ChefClaude/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import Header from "./Components/Header";
 import MainSection from "./Components/MainSection";
+import Counter from "./Components/Counter";
+import Practice from "./Components/Practice";
+import PracticeObjects from "./Components/PracticeObjects";
 
 export default function App() {
     return (
         <>
-            <Header />
-            <MainSection />
+            {/* <Header />
+            <MainSection /> */}
+            {/* <Counter /> */}
+            {/* <Practice /> */}
+            <PracticeObjects />
         </>
     );
 }
