56b9ecee4371422ed099327a3d93637c79a8155b
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-19 03:43:13 +0530

    Joke app with conditional rendering

--- React/ChefClaude/src/Components/PracticeCompos/ConditionalRenderingWithJokeCompo/jokesData.js
+++ React/ChefClaude/src/Components/PracticeCompos/ConditionalRenderingWithJokeCompo/jokesData.js
@@ -2,26 +2,30 @@ export default [
     {
         id: 1,
         setup: "I got my daughter a fridge for her birthday.",
-        punchline: "I can't wait to see her face light up when she opens it."
+        punchline: "I can't wait to see her face light up when she opens it.",
     },
     {
         id: 2,
         setup: "How did the hacker escape the police?",
-        punchline: "He just ransomware!"
+        punchline: "He just ransomware!",
     },
     {
         id: 3,
         setup: "Why don't pirates travel on mountain roads?",
-        punchline: "Scurvy."
+        punchline: "Scurvy.",
     },
     {
         id: 4,
         setup: "Why do bees stay in the hive in the winter?",
-        punchline: "Swarm."
+        punchline: "Swarm.",
     },
     {
         id: 5,
         setup: "What's the best thing about Switzerland?",
-        punchline: "I don't know, but the flag is a big plus!"
+        punchline: "I don't know, but the flag is a big plus!",
+    },
+    {
+        id: 6,
+        punchline: "Lol",
     }
-]
+];
