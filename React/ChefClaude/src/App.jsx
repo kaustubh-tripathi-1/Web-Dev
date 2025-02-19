88f0c6daebefcba04b72387eb8f478c15d300a28
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-19 06:45:43 +0530

    Practice Conditional Rendering

--- React/ChefClaude/src/App.jsx
+++ React/ChefClaude/src/App.jsx
@@ -5,16 +5,43 @@ import Counter from "./Components/PracticeCompos/Counter";
 import Practice from "./Components/PracticeCompos/Practice";
 import PracticeObjects from "./Components/PracticeCompos/PracticeObjects";
 import FormPractice from "./Components/PracticeCompos/FormPractice";
+import jokes from "./Components/PracticeCompos/ConditionalRenderingWithJokeCompo/jokesData.js";
+import Joke from "./Components/PracticeCompos/ConditionalRenderingWithJokeCompo/Joke.jsx";
 
 export default function App() {
+    /* //$ Render Jokes
+    const jokesList = jokes.map((joke) => (
+        <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />
+    )); */
+
+    //$ Conditional Rendering challenges
+    /* const [msgs, setMsgs] = useState([]);
+
+    function showNotification() {
+        if (msgs.length === 0) {
+            return `You're all caught up.`;
+        } else if (msgs.length === 1) {
+            return `You have ${msgs.length} unread msg.`;
+        } else {
+            return `You have ${msgs.length} unread msgs.`;
+        }
+    } */
+
     return (
         <>
-            {/* <Header />
-            <MainSection /> */}
+            <Header />
+            <MainSection />
             {/* <Counter /> */}
             {/* <Practice /> */}
             {/* <PracticeObjects /> */}
             {/* <FormPractice /> */}
+            {
+                //$ Conditional Rendering challenges
+                /* <main className="w-full bg-emerald-400 max-h-fit flex flex-col justify-center ">
+                {jokesList}
+                </main> */
+                // <h1>{showNotification()}</h1>
+            }
         </>
     );
 }
