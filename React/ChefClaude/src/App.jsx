9e1c2747a782542ef6992b8524ffe80916a356e1
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-21 01:22:57 +0530

    Sound Pads challenges to practice state being passed down to child compos

--- React/ChefClaude/src/App.jsx
+++ React/ChefClaude/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Header from "./Components/Header";
 import MainSection from "./Components/MainSection";
-import Counter from "./Components/PracticeCompos/Counter";
+import Counter from "./Components/PracticeCompos/Counter/Counter.jsx";
 import Practice from "./Components/PracticeCompos/Practice";
 import PracticeObjects from "./Components/PracticeCompos/PracticeObjects";
 import FormPractice from "./Components/PracticeCompos/FormPractice";
 import jokes from "./Components/PracticeCompos/ConditionalRenderingWithJokeCompo/jokesData.js";
 import Joke from "./Components/PracticeCompos/ConditionalRenderingWithJokeCompo/Joke.jsx";
+import Pad from "./Components/PracticeCompos/SoundPadsChallenge/Pad.jsx";
+import padsData from "./Components/PracticeCompos/SoundPadsChallenge/pads.js";
 
 export default function App() {
     /* //$ Render Jokes
@@ -27,10 +29,55 @@ export default function App() {
         }
     } */
 
+    //$ Sound Pads code
+    const [pads, setPads] = useState(padsData);
+
+    /* function toggle(id) {
+        setPads((prevPads) => {
+            const newPads = prevPads.map((obj) => {
+                if (obj.id === id) {
+                    const newObj = JSON.parse(JSON.stringify(obj));
+                    newObj.on = !newObj.on;
+                    return newObj;
+                }
+                return obj;
+            });
+
+            return newPads;
+        });
+    } */
+    /* function toggle(id) {
+        setPads((prevPads) =>
+            prevPads.map((obj) => {
+                return obj.id === id ? { ...obj, on: !obj.on } : obj;
+            }),
+        );
+    } */
+
+    /* function toggleButtons() {
+        setPads((prevPads) =>
+            prevPads.map((obj) => {
+                return { ...obj, on: obj.on ? false : true };
+            }),
+        );
+    } */
+
+    /* const padsList = pads.map((pad) => {
+        return (
+            <Pad
+                key={pad.id}
+                id={pad.id}
+                on={pad.on}
+                color={pad.color}
+                handleToggle={toggle}
+            />
+        );
+    }); */
+
     return (
         <>
-            <Header />
-            <MainSection />
+            {/* <Header />
+                        <MainSection /> */}
             {/* <Counter /> */}
             {/* <Practice /> */}
             {/* <PracticeObjects /> */}
@@ -38,10 +85,21 @@ export default function App() {
             {
                 //$ Conditional Rendering challenges
                 /* <main className="w-full bg-emerald-400 max-h-fit flex flex-col justify-center ">
-                {jokesList}
-                </main> */
+                            {jokesList}
+                            </main> */
                 // <h1>{showNotification()}</h1>
             }
+            {/* //$ Dynamic Styles challenges
+            <main className="grid h-full w-full grid-cols-[100px_100px] grid-rows-[repeat(4,100px)] items-start justify-evenly justify-items-center gap-4 bg-[#222222] p-14">
+                {padsList}
+                <button
+                    className="h-19 w-19 cursor-pointer bg-amber-500"
+                    type="button"
+                    onClick={toggleButtons}
+                >
+                    Turn off all buttons
+                </button>
+            </main> */}
         </>
     );
 }
--- React/ChefClaude/src/Components/PracticeCompos/SoundPadsChallenge/Pad.jsx
+++ React/ChefClaude/src/Components/PracticeCompos/SoundPadsChallenge/Pad.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-
-export default function Pad({ on, setPads, color }) {
-    const [isButtonOn, setIsButtonOn] = useState(on);
-
-    function handleButtonState() {
-        setIsButtonOn((prevState) => !prevState);
-    }
-
+export default function Pad({ id, on, handleToggle, color }) {
     return (
         <button
-            className={`h-full w-full cursor-pointer rounded-2xl border-2 border-rose-400 ${isButtonOn ? `opacity-[1]` : `opacity-[0.1]`}`}
+            className={`h-full w-full cursor-pointer rounded-2xl border-2 border-rose-400 ${on ? `opacity-[1]` : `opacity-[0.1]`}`}
             style={{ backgroundColor: color }}
             type="button"
-            onClick={handleButtonState}
+            title="Button"
+            onClick={() => handleToggle(id)}
         ></button>
     );
 }
