05cf801683e7c6eb0553ed4a1274102d679d201f
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-28 02:24:32 +0530

    Start Meme Generator Project to learn useEffect

---
+++ React/MemeGenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Header from "./Components/Header.jsx";
+import Main from "./Components/Main.jsx";
+import Practice from "./Components/PracticeChallenges/Practice.jsx";
+
+export default function App() {
+    return (
+        <>
+            {/* <Header />
+            <Main /> */}
+            <Practice />
+        </>
+    );
+}
