cd889720f3e45886314d91c1d40da9e32a02441e
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-28 03:05:26 +0530

    Create a small window size tracker

---
+++ React/MemeGenerator/src/Components/PracticeChallenges/WindowTracker.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function WindowTracker() {
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth);
+            setHeight(window.innerHeight);
+            // console.log(window.innerWidth);
+        }
+        window.addEventListener(`resize`, handleResize);
+
+        return () => {
+            window.removeEventListener(`resize`, handleResize);
+        };
+    }, [width]);
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold text-white">
+                Window Width : {width}
+            </h1>
+            <h1 className="text-2xl font-bold text-white">
+                Window Height : {height}
+            </h1>
+        </>
+    );
+}
