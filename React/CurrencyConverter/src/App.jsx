a11ad2bc569d87490cfa77d949c6db7c4a4d7808
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-01 08:25:52 +0530

    Update favicon

--- React/CurrencyConverter/src/App.jsx
+++ React/CurrencyConverter/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import bgImage from "./assets/bgImages/17454.jpg";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
+        <main className="h-full w-full bg-[url(./assets/bgImages/17454.jpg)] bg-cover bg-no-repeat bg-center">
             <h1>Hi</h1>
-        </>
+        </main>
     );
 }
 
