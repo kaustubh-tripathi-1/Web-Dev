005a6454e7c3c902717c9ba5b69c8b98007579fa
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-02 07:53:18 +0530

    Add custom hook to handle fetch currency

--- React/CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ React/CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,35 +1,59 @@
 import { useState, useEffect } from "react";
 
 export function useCurrencyInfo(currency) {
-    const baseUrl = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
-    const fallbackUrl = `https://latest.currency-api.pages.dev/v1/currencies/usd.json`;
+    const [currencyData, setCurrencyData] = useState(null);
+    const [error, setError] = useState(null);
 
-    console.log(baseUrl);
+    if (!currency) {
+        return [currencyData, error];
+    }
+
+    const baseUrl = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+    const fallbackUrl = `https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`;
 
     useEffect(() => {
         async function fetchCurrencyData(currency) {
-            // Base URL
+            //$ API call of Base URL
             try {
                 const response = await fetch(baseUrl);
 
+                console.log(response);
+
                 if (!response.ok) {
-                    throw new Error(`${response.status}`);
+                    throw new Error(
+                        `${response.status}, ${response.statusText}`
+                    );
                 }
 
                 const currencyJSON = await response.json();
+
+                console.log(currencyJSON[currency]);
+
+                setCurrencyData(currencyJSON[currency]);
             } catch (error) {
+                //$ API call of Fallback URL
                 try {
                     const response = await fetch(fallbackUrl);
 
                     if (!response.ok) {
-                        throw new Error(`${response.status}`);
+                        setError(true);
+                        throw new Error(
+                            `${response.status}, ${response.statusText}`
+                        );
                     }
 
                     const currencyJSON = await response.json();
+
+                    console.log(currencyJSON[currency]);
+
+                    setCurrencyData(currencyJSON[currency]);
                 } catch (error) {
                     console.error(error);
                 }
             }
         }
-    }, []);
+        fetchCurrencyData(currency);
+    }, [currency]);
+
+    return [currencyData, error];
 }
