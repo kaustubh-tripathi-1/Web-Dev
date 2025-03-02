83160acdda757dcdffdac8721be4ce9786a01e73
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-02 07:54:01 +0530

    Update UI

--- React/CurrencyConverter/src/App.jsx
+++ React/CurrencyConverter/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
-import bgImage from "./assets/bgImages/17454.jpg";
+import { useCurrencyInfo } from "./hooks/useCurrencyInfo.js";
+import InputCurrency from "./components/InputCurrency.jsx";
 
-function App() {
-    const [count, setCount] = useState(0);
+export default function App() {
+    const [currencyData, error] = useCurrencyInfo(null);
+
+    if (!error) {
+    }
 
     return (
-        <main className="h-full w-full bg-[url(./assets/bgImages/17454.jpg)] bg-cover bg-no-repeat bg-center">
-            <h1>Hi</h1>
+        <main className="h-full w-full bg-[url(./assets/bgImages/17454.jpg)] bg-cover bg-no-repeat bg-center opacity-100 flex justify-center items-center">
+            <section className="h-8/12 w-7/12 md:w-5/12 p-7 flex flex-col justify-center items-center gap-4 rounded-2xl backdrop-blur-md">
+                <InputCurrency />
+                <InputCurrency />
+                <button
+                    className="w-full h-1/6  text-white text-2xl rounded-xl cursor-pointer bg-gradient-to-r from-blue-800  to-blue-600  hover:bg-gradient-to-r hover:from-blue-700  hover:to-blue-500"
+                    type="button"
+                >
+                    Convert USD to INR
+                </button>
+            </section>
         </main>
     );
 }
-
-export default App;
