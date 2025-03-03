6d9632bec5903e31b67a591b6a264b1bf4f4f898
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-03 06:32:22 +0530

    Add API error indication

--- React/CurrencyConverter/src/App.jsx
+++ React/CurrencyConverter/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { useCurrencyInfo } from "./hooks/useCurrencyInfo.js";
 import InputCurrency from "./components/InputCurrency.jsx";
+import swapIcon from "./assets/sort.png";
 
 export default function App() {
     const [currencyData, error] = useCurrencyInfo(null);
+    const [fromCurrency, setFromCurrency] = useState();
 
-    if (!error) {
+    if (currencyData) {
+        const currencies = Object.keys(currencyData);
+        console.log(currencies);
     }
 
     return (
@@ -13,9 +17,14 @@ export default function App() {
             <section className="h-8/12 w-7/12 md:w-5/12 p-7 flex flex-col justify-center items-center gap-4 rounded-2xl backdrop-blur-md">
                 <InputCurrency />
                 <button
-                    className="w-20 h-10  bg-blue-600 hover:bg-blue-500 focus:bg-blue-500 outline-none text-white text-md absolute top-5/12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 rounded-xl cursor-pointer border-2 border-amber-300 hover:border-amber-500"
+                    className="w-22 h-10 bg-blue-600 hover:bg-blue-500 focus:bg-blue-500 outline-none text-white text-md absolute top-5/12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 rounded-xl cursor-pointer border-2 border-amber-300 hover:border-amber-500"
                     type="button"
                 >
+                    <img
+                        className=" w-5 inline -mt-1"
+                        src={swapIcon}
+                        alt="swap-icon"
+                    />{" "}
                     Swap
                 </button>
                 <InputCurrency />
@@ -25,6 +34,12 @@ export default function App() {
                 >
                     Convert USD to INR
                 </button>
+                {error && (
+                    <p className="w-full h-fit text-lg text-center text-red-600 font-semibold bg-amber-300 rounded-lg">
+                        There is an issue with the API. Please Try again
+                        later...
+                    </p>
+                )}
             </section>
         </main>
     );
