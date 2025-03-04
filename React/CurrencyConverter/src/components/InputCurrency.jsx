47e75aea212f339a113ae0049cb571413dd3e4d0
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-04 13:55:13 +0530

    Beautify Input and Select elements

--- React/CurrencyConverter/src/components/InputCurrency.jsx
+++ React/CurrencyConverter/src/components/InputCurrency.jsx
@@ -50,19 +50,19 @@ export default function InputCurrency({
 
     return (
         <div
-            className={`w-full min-h-40 p-3 sm:p-4 bg-white/90 shadow-md grid grid-cols-2 grid-rows-2 justify-between rounded-xl $transition-all duration-300 ease-in-out hover:bg-white ${className}`}
+            className={`w-full min-h-40 p-3 sm:p-4 bg-white/90 shadow-md grid grid-cols-2 grid-rows-2 justify-between rounded-xl $transition-all duration-300 ease-in-out hover:bg-white ${className} animate-fade-in`}
         >
             <label
                 htmlFor={forLabel}
-                className="text-gray-700 text-base sm:text-xl font-medium animate-pulse sm:w-1/4"
+                className="text-gray-700 text-base sm:text-xl font-medium sm:w-1/4"
             >
                 {forLabel === `from` ? "From" : "To"}
             </label>
-            <p className="text-gray-700 text-base sm:text-xl font-medium animate-fade-in justify-self-end">
+            <p className="text-gray-700 text-base sm:text-xl font-medium justify-self-end">
                 Currency Type
             </p>
             <input
-                className="md:w-9/6 self-center w-full sm:w-7/6 h-10 sm:h-12 px-3 bg-blue-100 hover:bg-blue-200 focus:bg-blue-200 focus:ring-2 focus:ring-blue-400 outline-none text-base sm:text-xl rounded-lg transition-colors duration-200 ease-in-out"
+                className="md:w-9/6 self-center w-full sm:w-7/6 h-10 sm:h-12 px-3 bg-gradient-to-r from-blue-300 to-blue-200 hover:from-blue-200 hover:to-blue-300 focus:ring-2 hover:bg-blue-200 focus:bg-blue-200 focus:ring-blue-400 outline-none text-base sm:text-xl rounded-lg transition-colors duration-200 ease-in-out"
                 type="number"
                 id={forLabel}
                 value={forLabel === `from` ? fromAmount : toAmount}
@@ -70,7 +70,7 @@ export default function InputCurrency({
             />
             {
                 <select
-                    className="w-5/6 sm:w-5/12 h-10 sm:h-12 bg-blue-100 hover:bg-blue-200 focus:bg-blue-200 focus:ring-2 focus:ring-blue-400 outline-none text-base sm:text-lg rounded-xl px-2 cursor-pointer transition-colors duration-200 ease-in-out self-center justify-self-end"
+                    className="w-5/6 sm:w-5/12 h-10 sm:h-12 bg-gradient-to-r from-blue-300 to-blue-200 hover:from-blue-200 hover:to-blue-300 focus:ring-2 hover:bg-blue-200 focus:bg-blue-200 focus:ring-blue-400 outline-none text-base sm:text-lg rounded-xl px-2 cursor-pointer transition-all duration-300 ease-in-out shadow-md justify-self-end self-center"
                     name={selectName}
                     value={
                         selectName === `from-select` ? fromCurrency : toCurrency
@@ -82,7 +82,7 @@ export default function InputCurrency({
                             <option
                                 value={currency}
                                 key={currency}
-                                className="text-base sm:text-lg"
+                                className="bg-gray-100 hover:bg-blue-300 text-gray-700 text-lg font-semibold p-2 sm:text-lg"
                             >
                                 {currency.toUpperCase()}
                             </option>
