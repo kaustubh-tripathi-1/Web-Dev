e12902f4799203323a3b9bab13d6cbe4d2bc501d
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-04 08:39:19 +0530

    Update handleOnChange, remove leading 0s in input

--- React/CurrencyConverter/src/components/InputCurrency.jsx
+++ React/CurrencyConverter/src/components/InputCurrency.jsx
@@ -1,14 +1,57 @@
 export default function InputCurrency({
-    fromCurrency /* = `usd` */,
-    toCurrency,
-    currencies,
+    currencies = [],
+    currencyData,
+    fromCurrency,
     setFromCurrency,
+    toCurrency,
     setToCurrency,
+    fromAmount,
+    setFromAmount,
+    toAmount,
+    setToAmount,
     selectName,
     forLabel,
+    className = ``,
 }) {
+    /**
+     * @param {Event} event
+     */
+    function handleOnChange(event) {
+        const inputValue = event.target.value;
+
+        if (inputValue === "") {
+            if (forLabel === "from") {
+                setFromAmount("");
+                setToAmount("");
+            } else {
+                setToAmount("");
+                setFromAmount("");
+            }
+            return;
+        }
+
+        const numericValue = Number(inputValue);
+        if (isNaN(numericValue) || numericValue < 0) return;
+
+        if (forLabel === "from") {
+            setFromAmount(numericValue);
+        } else {
+            setToAmount(numericValue);
+        }
+    }
+
+    function handleOnChangeSelect(e) {
+        if (selectName === "from-select") {
+            setFromCurrency(e.target.value);
+        } else {
+            setToCurrency(e.target.value);
+        }
+    }
+
     return (
-        <div className="w-full h-1/3 p-3 bg-white grid grid-cols-2 grid-rows-2 justify-between rounded-xl">
+        <div
+            className={`w-full h-5/12 p-3 bg-white grid grid-cols-2 grid-rows-2 justify-between rounded-xl ${className}`}
+        >
             <label
                 htmlFor={forLabel}
                 className="text-gray-700 text-xl justify-self-start"
@@ -22,6 +65,8 @@ export default function InputCurrency({
                 className="h-5/6 w-9/6 px-2 bg-blue-200 hover:bg-blue-300 focus:bg-blue-300 self-center outline-none text-xl rounded-xl "
                 type="number"
                 id={forLabel}
+                value={forLabel === `from` ? fromAmount : toAmount}
+                onChange={handleOnChange}
             />
             {
                 <select
@@ -30,15 +75,11 @@ export default function InputCurrency({
                     value={
                         selectName === `from-select` ? fromCurrency : toCurrency
                     }
-                    onChange={(e) => {
-                        selectName === `from-select`
-                            ? setFromCurrency(e.target.value)
-                            : setToCurrency(e.target.value);
-                    }}
+                    onChange={handleOnChangeSelect}
                 >
-                    {currencies.map((currency, index) => {
+                    {currencies.map((currency) => {
                         return (
-                            <option value={currency} key={index}>
+                            <option value={currency} key={currency}>
                                 {currency.toUpperCase()}
                             </option>
                         );
