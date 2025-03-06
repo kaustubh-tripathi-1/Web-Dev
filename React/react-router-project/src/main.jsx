ca4b11f9a5a313d586aab57207310184a724ad78
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-06 09:15:51 +0530

    Learn React Router with Project

---
+++ React/react-router-project/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </Router>
+    </StrictMode>
+);
