2e4f333032410010097e5cb52be1981d8bb55563
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-07 09:53:36 +0530

    Add Header and Footer compos which persists along diff. routes

--- React/react-router-project/src/components/header/Header.jsx
+++ React/react-router-project/src/components/header/Header.jsx
@@ -27,10 +27,10 @@ export default function Header() {
                         </Link>
                     </div>
                     <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        className="hidden justify-between items-center w-full md:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
                     >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                        <ul className="flex flex-col mt-4 font-medium md:flex-row md:w-fit lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
                                     to="/"
