fbb2b01616e5aede4381dcbf1d6dc829de00004e
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-07 09:52:08 +0530

    Create routes in main.jsx itself with <Layout />

--- React/react-router-project/src/main.jsx
+++ React/react-router-project/src/main.jsx
@@ -1,15 +1,51 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router";
 import App from "./App.jsx";
+import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
+import Layout from "./components/layout/Layout.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Github from "./components/github/Github.jsx";
+import User from "./components/user/User.jsx";
+
+//$ 1st way to define routes
+/* const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { path: "", element: <Home /> },
+            { path: "about", element: <About /> },
+            { path: "contact", element: <Contact /> },
+            { path: "github", element: <Github /> },
+        ],
+    },
+]); */
+
+//$ 2nd way to define routes
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/github" element={<Github />} />
+            <Route path="/user/:userId" element={<User />} />
+        </Route>
+    )
+);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<App />} />
-            </Routes>
-        </Router>
+        <RouterProvider router={router} />
     </StrictMode>
 );
