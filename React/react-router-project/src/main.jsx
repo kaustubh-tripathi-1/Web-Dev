f945745e499137681113874558dd252f7996bbd2
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-08 19:24:35 +0530

    Use loader fn. to fetch data before the compo is rendered instead of using useEffect

--- React/react-router-project/src/main.jsx
+++ React/react-router-project/src/main.jsx
@@ -15,6 +15,7 @@ import About from "./components/about/About.jsx";
 import Layout from "./components/layout/Layout.jsx";
 import Contact from "./components/contact/Contact.jsx";
 import Github from "./components/github/Github.jsx";
+import { fetchGithubData } from "./components/github/fetchGithubData.js";
 import User from "./components/user/User.jsx";
 
 //$ 1st way to define routes
@@ -26,7 +27,7 @@ const router = createBrowserRouter([
             { path: "", element: <Home /> },
             { path: "about", element: <About /> },
             { path: "contact", element: <Contact /> },
-            { path: "github", element: <Github /> },
+            { path: "github", element: <Github />, loader: fetchGithubData },
             { path: "user/:userId", element: <User /> },
         ],
     },
