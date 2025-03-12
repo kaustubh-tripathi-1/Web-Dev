78237a085f76b3991165a64b8817db663ae8e7d3
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-12 22:31:26 +0530

    Add MainContent compo and wrap everything in it so that ThemeProvider gets called before using theme state

--- React/context-api-theme-changer/src/App.jsx
+++ React/context-api-theme-changer/src/App.jsx
@@ -1,33 +1,10 @@
-import { useState, createContext, useEffect } from "react";
-import { ThemeContext /* , useTheme */ } from "./contexts/ThemeContext";
-import ThemeBtn from "./components/ThemeBtn";
-import Card from "./components/Card";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+import MainContent from "./components/MainContent";
 
 export default function App() {
-    const [theme, setTheme] = useState(`light`);
-
-    /* function changeTheme() {
-        setTheme((prevTheme) => (prevTheme === `light` ? `dark` : `light`));
-    } */
-
-    useEffect(() => {
-        const html = document.querySelector(`html`);
-        html.classList.remove(`light`, `dark`);
-        html.classList.add(theme);
-    }, [theme]);
-
     return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            <main className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        <ThemeBtn />
-                    </div>
-                    <div className="w-full max-w-sm mx-auto">
-                        <Card />
-                    </div>
-                </div>
-            </main>
-        </ThemeContext.Provider>
+        <ThemeProvider>
+            <MainContent />
+        </ThemeProvider>
     );
 }
