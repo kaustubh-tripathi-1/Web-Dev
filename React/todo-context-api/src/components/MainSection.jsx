5a6aa7a4097f6074ac0c67cbf1b2f7b4cf016567
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-16 02:00:48 +0530

    Add clearAllTodos functionality

--- React/todo-context-api/src/components/MainSection.jsx
+++ React/todo-context-api/src/components/MainSection.jsx
@@ -3,11 +3,11 @@ import TodoForm from "./ToDoForm.jsx";
 import TodoItem from "./ToDoItem.jsx";
 
 export default function MainSection() {
-    const { todos } = useToDo();
+    const { todos, clearAllTodos } = useToDo();
 
     return (
         <div className="bg-[#172842] min-h-screen py-8">
-            <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+            <div className="w-full h-fit max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white flex flex-col justify-center gap-4">
                 <h1 className="text-2xl font-bold text-center mb-8 mt-2">
                     Manage Your To-Dos
                 </h1>
@@ -24,6 +24,15 @@ export default function MainSection() {
                         />
                     ))}
                 </div>
+                {todos.length > 0 && (
+                    <button
+                        type="button"
+                        className="cursor-pointer h-10 w-1/2 self-center bg-red-400 hover:bg-[#ff4949] rounded-xl"
+                        onClick={clearAllTodos}
+                    >
+                        Clear all To-Dos
+                    </button>
+                )}
             </div>
         </div>
     );
