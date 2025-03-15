23eb997c106273a5ae436102c6027449c6576e79
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-15 16:52:47 +0530

    Add all functions

--- React/todo-context-api/src/components/ToDoProvider.jsx
+++ React/todo-context-api/src/components/ToDoProvider.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToDoContext } from "../contexts/ToDoContext.js";
 
 export default function ToDoProvider({ children }) {
     const [todos, setTodos] = useState([]);
 
+    //$ All functions to be added to context
+    //@ Adds a task to the todos state
     function addTask(task) {
         setTodos((prevTodos) => [
             ...prevTodos,
@@ -15,11 +17,47 @@ export default function ToDoProvider({ children }) {
         ]);
     }
 
-    function updateTask(id, task) {}
+    //@ Updates the task with the id in the todos state using map()
+    function updateTask(id, task) {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) => {
+                return todo.id === id ? { ...todo, task: task } : todo;
+            })
+        );
+    }
 
-    function deleteTask(id) {}
+    //@ Deletes the task with the id in the todos state using filter()
+    function deleteTask(id) {
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    }
 
-    function toggleTaskCompleted(id) {}
+    //@ Toggles the completion of task with the id in the todos state using map() and overwriting the completed property
+    function toggleTaskCompleted(id) {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    }
+
+    function clearAllTodos() {
+        setTodos([]);
+        localStorage.clear();
+    }
+
+    //@ 1st side effect - to get all the stored todos from the local storage
+    useEffect(() => {
+        const localTodos = JSON.parse(localStorage.getItem(`todos`));
+
+        if (localTodos && localTodos.length) {
+            setTodos(localTodos);
+        }
+    }, []);
+
+    //@ 2nd side effect - to set all the todos which are in the state todos to the local storage
+    useEffect(() => {
+        localStorage.setItem(`todos`, JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <ToDoContext.Provider
@@ -29,6 +67,7 @@ export default function ToDoProvider({ children }) {
                 updateTask,
                 deleteTask,
                 toggleTaskCompleted,
+                clearAllTodos,
             }}
         >
             {children}
