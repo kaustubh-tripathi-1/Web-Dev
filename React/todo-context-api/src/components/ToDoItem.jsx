5a9cf93e668da47886da9a47f20a0be7b14bf7b6
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-16 02:01:37 +0530

    Change initial value of isTodoEditable to false

--- React/todo-context-api/src/components/ToDoItem.jsx
+++ React/todo-context-api/src/components/ToDoItem.jsx
@@ -4,17 +4,17 @@ import { useToDo } from "../hooks/useToDo";
 export default function TodoItem({ id, task, completed }) {
     const { updateTask, deleteTask, toggleTaskCompleted } = useToDo();
 
-    const [isTodoEditable, setIsTodoEditable] = useState(completed);
+    const [isTodoEditable, setIsTodoEditable] = useState(false);
     const [toDoMsg, setTodoMsg] = useState(task);
 
     return (
         <div
-            className={`w-full flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+            className={`w-full h-fit flex items-center justify-evenly border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
                 completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
             }`}
         >
             <label
-                htmlFor="mark-as-completed"
+                htmlFor={`mark-as-completed-${id}`}
                 className="cursor-pointer text-xs whitespace-nowrap flex items-center"
             >
                 Mark as done
@@ -24,13 +24,14 @@ export default function TodoItem({ id, task, completed }) {
                 className="cursor-pointer"
                 checked={completed}
                 onChange={() => toggleTaskCompleted(id)}
-                id="mark-as-completed"
+                id={`mark-as-completed-${id}`}
             />
             <input
                 type="text"
-                className={`whitespace-pre-wrap h-fit border outline-none w-full bg-transparent rounded-lg ${
+                name="task"
+                className={`h-fit text-wrap border outline-none w-full bg-transparent rounded-lg ${
                     isTodoEditable
-                        ? "border-black/10 px-2"
+                        ? "border-black/10 px-2 bg-yellow-200"
                         : "border-transparent"
                 } ${completed ? "line-through" : ""}`}
                 value={toDoMsg}
@@ -39,20 +40,20 @@ export default function TodoItem({ id, task, completed }) {
             />
             {/* Edit, Save Button */}
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50 cursor-pointer"
+                className={`inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50 ${
+                    completed ? "cursor-default" : "cursor-pointer"
+                }`}
                 onClick={() => {
                     if (completed) return;
 
                     if (isTodoEditable) {
                         updateTask(id, toDoMsg);
-                        setIsTodoEditable((prev) => !prev);
-                    } else {
-                        setIsTodoEditable((prev) => !prev);
                     }
+                    setIsTodoEditable((prev) => !prev);
                 }}
                 disabled={completed}
             >
-                {isTodoEditable ? "📁" : "✏️"}
+                {completed ? "✔️" : isTodoEditable ? "📁" : "✏️"}
             </button>
             {/* Delete Todo Button */}
             <button
