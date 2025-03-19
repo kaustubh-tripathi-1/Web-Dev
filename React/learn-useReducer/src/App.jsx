823f2ff01b3a18c39fd70a5238c25d38ccd4f751
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-19 22:34:23 +0530

    Add ThemeSwitcher compo with useReducer

--- React/learn-useReducer/src/App.jsx
+++ React/learn-useReducer/src/App.jsx
@@ -2,11 +2,13 @@ import Counter from "./components/Counter";
 import "./App.css";
 import TaskManager from "./components/TaskManager";
 import ShoppingCart from "./components/ShoppingCart";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
 export default function App() {
     return (
         // <Counter />
         // <TaskManager />
-        <ShoppingCart />
+        // <ShoppingCart />
+        <ThemeSwitcher />
     );
 }
