4a955fad67c5a9e95aa2ada9424e236e9606c181
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-19 17:55:18 +0530

    Add ShoppingCart compo with useReducer

---
+++ React/learn-useReducer/src/components/ShoppingCart.jsx
@@ -0,0 +1,12 @@
+import "./ShoppingCart.css";
+
+function cartReducer(state, action) {}
+
+export default function ShoppingCart() {
+    return (
+        <main className="main-container">
+            <h1>Shopping Cart</h1>
+            <ul>{/* Map over items and display them here */}</ul>
+        </main>
+    );
+}
