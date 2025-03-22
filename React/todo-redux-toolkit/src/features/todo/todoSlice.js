7354c2bf7cb2dfb7c4c32cda7369922bd13a30a7
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-22 05:33:38 +0530

    Add state, reducers and actions in the todos slice

--- React/todo-redux-toolkit/src/features/todo/todoSlice.js
+++ React/todo-redux-toolkit/src/features/todo/todoSlice.js
@@ -1,14 +1,54 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+    todos: {},
+    order: [],
+};
 
 const todoSlice = createSlice({
     name: "todos",
     initialState,
     reducers: {
-        addTodo: (state, action) => {},
-        updateTodo: (state, action) => {},
-        deleteTodo: (state, action) => {},
-        clearTodos: (state, action) => {},
+        addTask: (state, action) => {
+            const uniqueId = nanoid();
+            state.todos[uniqueId] = {
+                id: uniqueId,
+                task: action.payload.task,
+                completed: false,
+            };
+            state.order.push(uniqueId);
+        },
+        updateTask: (state, action) => {
+            const { id, task } = action.payload;
+            if (state.todos[id]) {
+                state.todos[id].task = task;
+            }
+        },
+        deleteTask: (state, action) => {
+            const { id } = action.payload;
+            if (state.todos[id]) {
+                delete state.todos[id];
+                state.order = state.order.filter((orderId) => orderId !== id);
+            }
+        },
+        toggleTaskCompleted: (state, action) => {
+            const { id } = action.payload;
+            if (state.todos[id]) {
+                state.todos[id].completed = !state.todos[id].completed;
+            }
+        },
+        clearAllTodos: (state, action) => {
+            state.todos = {};
+            state.order = [];
+        },
     },
 });
+
+export const {
+    addTask,
+    updateTask,
+    deleteTask,
+    toggleTaskCompleted,
+    clearAllTodos,
+} = todoSlice.actions;
+export default todoSlice.reducer;
